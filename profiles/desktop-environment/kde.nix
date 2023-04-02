e100adffd454f4abd0b60b87923a39c09dbc39b3
I-Want-ToBelieve <floatshuyin@gmail.com>
2023-04-03 00:45:25 +0800

    chore: update

--- profiles/desktop-environment/kde.nix
+++ profiles/desktop-environment/kde.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}: {
   services.xserver.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+
+  # @see https://github.com/NixOS/nixpkgs/issues/75867#issuecomment-949542936
   programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.ksshaskpass.out}/bin/ksshaskpass";
+
+  # @see https://github.com/Prayag2/kde_controlcentre/issues/6#issuecomment-1271939571
+  environment.systemPackages = with pkgs; [kdeplasma-addons];
 }
