cef557847383793da21908287d272fab2813be80
I-Want-ToBelieve <floatshuyin@gmail.com>
2023-05-16 20:41:55 +0800

    chore: warframe hosts

--- profiles/network.nix
+++ profiles/network.nix
@@ -16,11 +16,13 @@
       # https://docs.zichou.eu.org/wfhosts
 
       # 解决登录问题
-      205.185.216.10 content.warframe.com
+      205.185.216.42 content.warframe.com
 
       # 香港 (国内裸连 + 加速器)
-      23.41.68.161 origin.warframe.com
-      23.41.68.161 api.warframe.com
+      23.194.224.167 origin.warframe.com
+      23.194.224.167 api.warframe.com
+      23.194.224.167 arbiter.warframe.com
+      # 23.35.126.124
 
       # 编辑日期: 2023-05-14
     '';
